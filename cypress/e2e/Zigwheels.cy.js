1c353d008f9f8df8d0180eae032090c8642bb2ec
Santhosh-Kumar-Sankaralingam <2400347@cognizant.com>
2025-06-06 14:48:20 +0530

    cap2

--- cypress/e2e/Zigwheels.cy.js
+++ cypress/e2e/Zigwheels.cy.js
@@ -1,7 +1,4 @@
-
-
-import './NewBikesPage1.cy.js';
-import './NewsAndReviews1.cy.js';
-import './NewScooters1.cy.js';
-import './UsedCarsPage1.cy.js';
-// import './loginPage.cy.js';
+import './NewBikesPage.cy.js';
+import './NewScooters.cy.js';
+import './NewsAndReviews.cy.js';
+import './UsedCarsPage.cy.js';
